1ea0cd3cddad7deabefa58ce5621fc350db38f06
Thomas <thomasjk10@gmail.com>
2018-10-09 19:06:05 +0530

    Update Is_Unique.py

--- Chapter 1/Is_Unique.py
+++ Chapter 1/Is_Unique.py
@@ -1,18 +1,17 @@
-#Implement an algorithm to determine if a string has all unique characters. What if you
-#cannot use additional data structures?
-
 def unq_char(string):
-    # Assuming character set contains only ASCII
+    # Assuming the entire string contains only ASCII
     # There can't be more than 128 unique ASCII char in a string, hence the below check
     if len(string) > 128:
         return False
 
     str_list = [ord(c) for c in string]
+    # Filter out unique characters in the list
     unq_list = set(str_list)
     if len(str_list) != len(unq_list):
         return False
-
-Test = "hb 627jh=j ()"
+# Test Cases - Test = "abcd123 e", Test = "abc@$a% 1de)"
+Test = "abcde"
 
 Chk = unq_char(Test)
+print (Chk)
 
