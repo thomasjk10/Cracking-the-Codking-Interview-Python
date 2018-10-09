54eeb7af02b54db161bd1190a6a68a714010ed5c
Thomas <thomasjk10@gmail.com>
2018-10-09 21:17:16 +0530

    Update 2_Check_permut.py

--- Chapter 1/2_Check_permut.py
+++ Chapter 1/2_Check_permut.py
@@ -1,4 +1,6 @@
+# ------------------------------------------------------------------------------
 # Approach 1 - Sort both the given strings and then compare if they are the same
+# ------------------------------------------------------------------------------
 def chk_permu(s1,s2):
     # Assuming both strings contain only ASCII
     # There can't be more than 128 unique ASCII char in a string, hence the below check
@@ -11,6 +13,36 @@ def chk_permu(s1,s2):
 
 # Test Case1 - str1 = "abcd", str2 = "dbca"
 # Test Case2 - str1 = "abc d", str2 = "cdba"
+str1 = input()
+str2 = input()
+result = chk_permu(str1,str2)
+
+# ------------------------------------------------------------------------------
+# Approach 2 - Check if length of counter dictionary varies after processing
+#              the first occurence or a char in the second string when length of  
+#              dictinary increases 
+# ------------------------------------------------------------------------------
+
+from collections import Counter
+def chk_permu(s1,s2):
+    if len(s1) > 128 or len(s2) > 128:
+        return False
+
+    if len(s1) != len(s2):
+        return False
+
+    char_count = Counter()
+    for char in s1:
+        char_count[char] += 1
+    init_len = len(char_count)
+    for char in s2:
+        char_count[char] += 1
+        fin_len = len(char_count)
+        if fin_len - init_len != 0:
+            return False
+
+    return True
+
 str1 = input()
 str2 = input()
 result = chk_permu(str1,str2)
