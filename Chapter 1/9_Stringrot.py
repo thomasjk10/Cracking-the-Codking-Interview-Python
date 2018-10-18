1cd3b7a3b3caa6a181cdf0f7a70f40c5323c8471
Thomas <thomasjk10@gmail.com>
2018-10-19 02:36:28 +0530

    Update 9_Stringrot.py

--- Chapter 1/9_Stringrot.py
+++ Chapter 1/9_Stringrot.py
@@ -1,1 +1,14 @@
+''' String compression'''
 
+def is_substring(longstr, substr):
+    return longstr.find(substr)
+
+def strrotation(s1, s2):
+    if len(s1) == len(s2) != 0:
+        return is_substring(s1 + s1, s2) != -1
+    return False
+
+string1 = input()
+string2 = input()
+result = strrotation(string1,string2)
+print (result)
